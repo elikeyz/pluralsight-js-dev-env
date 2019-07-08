574364f8367fba8cfee2ac09b799df1b3e54bb5e
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-07-08 22:38:26 +0100

    setup production deployment

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://desolate-gorge-75079.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
